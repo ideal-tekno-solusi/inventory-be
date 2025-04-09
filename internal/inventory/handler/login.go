9ed663b05100963678ca0be844e19409c5ba6565
Ryuze <alfianyunianto74@gmail.com>
2025-04-09 19:23:20 +0700

    update code for dev sso

--- internal/inventory/handler/login.go
+++ internal/inventory/handler/login.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/spf13/viper"
 )
 
 func (r *RestService) Login(ctx *gin.Context) {
@@ -25,9 +26,39 @@ func (r *RestService) Login(ctx *gin.Context) {
 	hash := sha256.New()
 
 	hash.Write(codeVerifier)
-
 	codeChallenge := base64.StdEncoding.EncodeToString(hash.Sum(nil))
 
+	//? set cookie httponly for code verifier, untuk dev di set ke 1 jam
+	ctx.SetCookie("INVENTORY-CODE-VERIFIER", string(codeVerifier), 3600, "/", "localhost", false, true)
+
+	//TODO: below create new csrf token, ignore the middleware one and replace it with this new token
+	age := viper.GetInt("config.csrf.age")
+	domain := viper.GetString("config.csrf.domain")
+	path := viper.GetString("config.csrf.path")
+
+	key := make([]byte, 32)
+	_, err = rand.Read(key)
+	if err != nil {
+		utils.SendProblemDetailJson(ctx, http.StatusInternalServerError, err.Error(), ctx.FullPath(), uuid.NewString())
+
+		return
+	}
+
+	csrfToken := base64.StdEncoding.EncodeToString(key)
+
+	ctx.SetCookie("INVENTORY-XSRF-TOKEN", csrfToken, age, path, domain, false, false)
+
+	type test struct {
+		Csrf      string `json:"csrf"`
+		Challenge string `json:"challenge"`
+	}
+
+	res := test{
+		Csrf:      csrfToken,
+		Challenge: codeChallenge,
+	}
+
 	//TODO: set redirect to sso be /authorization, ex: /auth?redirect=client/homepage&client_id=client&response_type=code&scopes=profile email&state=csrf-example-client&code_challenge={challenge}&code_challenge_method=S256
-	ctx.Redirect(http.StatusPermanentRedirect, "http://localhost:8080/v1/api/category")
+	// ctx.Redirect(http.StatusPermanentRedirect, "http://localhost:8080/v1/api/category")
+	ctx.JSON(200, res)
 }
