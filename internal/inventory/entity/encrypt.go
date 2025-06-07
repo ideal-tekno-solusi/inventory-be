f9134bf70c5089237816217838301eb7a7fd937a
Ryuze <alfianyunianto74@gmail.com>
2025-06-07 23:12:08 +0700

    remove unused entity

--- internal/inventory/entity/encrypt.go
+++
@@ -1,7 +0,0 @@
-package entity
-
-type CodeMessage struct {
-	CodeChallenge       string `json:"codeChallenge"`
-	CodeChallengeMethod string `json:"codeChallengeMethod"`
-	AuthorizationCode   string `json:"authorizationCode"`
-}
